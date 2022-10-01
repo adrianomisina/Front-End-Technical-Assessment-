6d8d246a26c54eb7fb7a1a694f95c71cd2fc8013
Adriano Misina <adriano.misina@gmail.com>
2022-09-30 21:15:54 -0300

    second commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import InviteImg from '../../assets/invite.png'
 
 const Home = () => {
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.container_grid}>
         <div className={styles.grid_invite}>
           <img src={InviteImg} alt="hero-home" />
@@ -12,18 +12,16 @@ const Home = () => {
 
         <div className={styles.grid_heroAction}>
           <div className={styles.heading_text}>
-          <h1>Imagine if<br/>
-            <span>Snapchat</span> <br/>
-              had events.
-          </h1>
-          <p>Easily host and share events with your friends across any social media.</p>
-          <button>🎉 Create my event</button>
+            <h1>Imagine if<br/>
+              <span>Snapchat</span> <br/>
+                had events.
+            </h1>
+            <p>Easily host and share events with your friends across any social media.</p>
+            <button className={styles.btn} >🎉 Create my event</button>
           </div>
-
-
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
